0c6ecb433bce0527d915213b99e03e4f1d879d5d
epiccoding <epickhart@gmail.com>
2023-06-05 15:05:15 -0400

    I think I'm really getting somewhere

--- Project3/Streamlit/openbb.py
+++ Project3/Streamlit/openbb.py
@@ -2,73 +2,76 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
-from datetime import date
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 from openbb_terminal.stocks.stocks_helper import load
 from openbb_terminal.common.feedparser_model import get_news
 
+default_start = (date.today() - relativedelta(months=6)).strftime('%Y-%m-%d')
+default_end = (date.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+
+def user_iput():
+    # User input to select the Ticker for the view
+    ticker = st.sidebar.text_input(f"Enter a Ticker Symbol", value="AAPL")
+    # Choose how many articles you want, default 25
+    limit = st.sidebar.slider('How many articles?', 0, 100, 25)
+    # Time logic
+    start = st.sidebar.text_input("Start Date", default_start)
+    end = st.sidebar.text_input("End Date", default_end)
+    return ticker, limit, start, end
+
+ticker, limit, start, end = user_iput()
+
+# News Function
 @st.cache_data
 def news(ticker):
     # OpenBB calling code for news
-    news_df = get_news(term=ticker, limit=50)
+    news_df = get_news(term=ticker, limit=limit)
     # Ensures date is formatted conveniently
     news_df['Date'] = pd.to_datetime(news_df['Date']).dt.strftime('%Y-%m-%d')
     return news_df
 
-@st.cache_data
-def get_close(ticker):
-    # Grab closing price data
-    end_date = pd.Timestamp(date.today()) + pd.DateOffset(days=1)
-    start_date = pd.Timestamp(date.today()) - pd.DateOffset(months=6) # Get a little bit extra in case of weekend
-    close = load(symbol=ticker, start_date=start_date, end_date=end_date)
-    close_df = pd.DataFrame({'Date': close.index, 'Open': close['Open'], 'Close': close['Close']})
-    close_df['Date'] = pd.to_datetime(close_df['Date']).dt.strftime('%Y-%m-%d')
-    return close_df
+start = pd.to_datetime(start).strftime('%Y-%m-%d')
+end = pd.to_datetime(end).strftime('%Y-%m-%d')
 
-# Streamlit app code
-def main():
-    st.title("News App")
-    # User input Ticker symbol here
-    ticker = st.sidebar.text_input(f"Enter a Ticker Symbol", value="AAPL")
-    # If one selected then:
-    if ticker:
-        chart_df = get_close(ticker)
-        if not chart_df.empty:
-            current_price = chart_df['Close'].iloc[-1]
-            previous_price = chart_df['Close'].iloc[-2]
-            price_change = current_price - previous_price
+# Grab closing price data
+close = load(symbol=ticker, start_date=start, end_date=end)
+close_df = pd.DataFrame({'Date': close.index, 'Open': close['Open'], 'Close': close['Close']})
+close_df['Date'] = pd.to_datetime(close_df['Date']).dt.strftime('%Y-%m-%d')
 
-            if price_change > 0:
-                color = "green"
-            elif price_change < 0:
-                color = "red"
-            else:
-                color = "black"
+# Color coded Close Price Changer
+price_df = close
+if not price_df.empty:
+    current_price = price_df['Close'].iloc[-1]
+    previous_price = price_df['Close'].iloc[-2]
+    price_change = current_price - previous_price
+    if price_change > 0:
+        color = "green"
+    elif price_change < 0:
+        color = "red"
+    else:
+        color = "black"
+    st.sidebar.markdown(f"Currently: <span style='color:{color}; font-weight:bold;'>${current_price:.2f} ({price_change:.2f})</span>", unsafe_allow_html=True)
+else:
+    st.sidebar.write("No close price to report")
 
-            st.sidebar.markdown(f"Currently: <span style='color:{color}; font-weight:bold;'>${current_price:.2f} ({price_change:.2f})</span>", unsafe_allow_html=True)
+# SIDEBAR #
+st.sidebar.line_chart(price_df["Close"])
 
-            st.sidebar.markdown(f"**6 Month Close Price**")
-            fig, ax = plt.subplots()
-            chart_df.plot(x='Date', y='Close', ax=ax)
-            ax.set_xlabel("")
-            ax.set_ylabel("")
-            plt.xticks([])
-            st.sidebar.pyplot(fig)
 
-        else:
-            st.sidebar.write("No close price to report")
+# MAIN BODY #
+st.title("Snapshot")
 
-        st.markdown(f"Fetching news for {ticker}...  While you wait, here is an inspirational quote:")
-        st.markdown(f"""
-            >You're only given a little spark of madness. You mustn't lose it. -**:blue[Robin Williams]**"""
-        )
+# Column Structure
+col1, col2, col3 = st.columns([1, 1, 3])
 
-        # This will print out the captured OpenBB data
-        news_df = news(ticker)
-        if len(news_df) > 0:
-            st.write(f"Found {len(news_df)} news articles for {ticker}:")
-            st.write(news_df)
-        else:
-            st.write("No news articles found for the given ticker.")
 
-if __name__ == '__main__':
-    main()
+with col3:
+    st.header("News Articles")
+    # This will print out the captured OpenBB data
+    news_df = news(ticker)
+    if len(news_df) > 0:
+        st.write(f"Found {len(news_df)} news articles for {ticker}:")
+        st.write(news_df)
+    else:
+        st.write("No news articles found for the given ticker.")
