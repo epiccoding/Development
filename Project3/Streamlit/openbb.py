54fdcddafa274dec245a16a9c0398d0f01da451f
epiccoding <epickhart@gmail.com>
2023-06-05 11:19:38 -0400

    maybe full import openbb is too much

--- Project3/Streamlit/openbb.py
+++ Project3/Streamlit/openbb.py
@@ -3,6 +3,6 @@ import pandas as pd
 import requests
 import os
 import sys
-from openbb_terminal.sdk import openbb
+from openbb_terminal.stocks.stocks_helper import load
 
 st.markdown('Hello World')
